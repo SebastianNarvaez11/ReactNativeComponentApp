25668d2b1f38e3c99a1566e961eb0e311037dfc8
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2024-03-19 11:58:27 -0500

    add persists async storage theme with zustan

--- src/presentation/store/themeStore.ts
+++ src/presentation/store/themeStore.ts
@@ -1,5 +1,7 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {create} from 'zustand';
 import {ThemeColors, darkColors, lightColors} from '../../config/theme/theme';
+import {createJSONStorage, persist} from 'zustand/middleware';
 
 export type ThemeType = 'light' | 'dark';
 
@@ -11,19 +13,27 @@ interface IThemeStore {
   setIsSystemTheme: (status: boolean) => void;
 }
 
-export const useThemeStore = create<IThemeStore>()(set => ({
-  currentTheme: 'light',
-  isSystemTheme: false,
-  colors: lightColors,
+export const useThemeStore = create<IThemeStore>()(
+  persist(
+    set => ({
+      currentTheme: 'light',
+      isSystemTheme: false,
+      colors: lightColors,
 
-  setTheme: (theme: ThemeType) => {
-    set({
-      currentTheme: theme,
-      colors: theme === 'light' ? lightColors : darkColors,
-    });
-  },
+      setTheme: (theme: ThemeType) => {
+        set({
+          currentTheme: theme,
+          colors: theme === 'light' ? lightColors : darkColors,
+        });
+      },
 
-  setIsSystemTheme: (status: boolean) => {
-    set({isSystemTheme: status});
-  },
-}));
+      setIsSystemTheme: (status: boolean) => {
+        set({isSystemTheme: status});
+      },
+    }),
+    {
+      name: 'theme-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
